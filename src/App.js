516f11681aa2ba07231aa219d674188433e2a528
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2024-12-26 09:48:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import researchpaper from "./AddictiveAuctions.pdf"
+function App() {
+  return (
+    <div>
+    <center><h1><b><i> Economist's Life </i></b></h1></center>
+   <br/>
+   <div class="middle">
+     <ol>
+    <li>
+    <b><u><a href={researchpaper} target="_blank" rel="noopener noreferrer" style={{color:"black"}}>Addictive auctions: Using lucky-draw and gambling addiction to increase participation during auctioning</a></u></b>,<br/>
+     DOI Number: <a href="https://doi.org/10.51483/IJMRE.1.1.2021.68-74" target="_blank" rel="noopener noreferrer">10.51483/IJMRE.1.1.2021.68-74</a>
+    
+     <p align="justify">
+     <b>Abstract</b>: Auction theories are believed to provide a better selling opportunity for the resources to be allocated. Various organizations have took measures to increase trust among participants towards their auction system, but trust alone cannot ensure a high level of participation. We propose a new type of auction system which takes advantage of lucky draw and gambling addictions to increase the engagement level of candidates in an auction. Our system makes use of security features present in existing auction systems for ensuring fairness and maintaining trust among participants.
+     </p>
+
+     <p align="justify">
+     <b>Cite as</b>: Addictive auctions: Using lucky-draw and gambling addiction to increase participation during auctioning. International Journal of Management Research and Economics. 1(1),68-74. doi: 10.51483/IJMRE.1.1.2021.68-74.
+     </p>
+
+     <p align="justify">
+     <u>Resources</u>:
+     <br/>
+     <ol>
+     <li>Published paper: <a target="_blank" rel="noopener noreferrer" href="https://www.svedbergopen.com/files/1612268008_(5)_IJMRE28112020MTN007_(p_68-74).pdf">click here</a></li>
+     <li>Published paper (DOI Link): <a target="_blank" rel="noopener noreferrer" href="https://doi.org/10.51483/IJMRE.1.1.2021.68-74">click here</a></li>
+     <li>Arxiv paper: <a target="_blank" rel="noopener noreferrer" href="https://arxiv.org/abs/1906.03237">click here</a></li>
+     <li>Fetch paper from github: <a target="_blank" rel="noopener noreferrer" href={researchpaper}>click here</a></li>
+     <li>Github repository: <a target="_blank" rel="noopener noreferrer" href="https://github.com/mr-ravin/collaborative-auction">source code</a></li>
+     </ol>
+     </p>
+
+    </li>
+    </ol>
+    
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
